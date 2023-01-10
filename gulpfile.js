4768a1f6b2cc9fec60c52a39bcb9234b7c6ef7f8
Hungry_munk <abbaszadeharsam@gmail.com>
2023-01-10 17:12:10 +1100

    installed sass and gulp packages

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const browserSync = require("browser-sync").create();
+
+function styles(done) {
+    // Specify path of source SASS / SCSS file
+    return (
+        gulp
+            .src("./sass/**/*.scss")
+            // Compile it, and catch errors
+            .pipe(sass().on("error", sass.logError))
+            // Save the CSS output file in this path
+            .pipe(gulp.dest("./dist"))
+            // Auto-inject into browsers
+            .pipe(browserSync.stream())
+    );
+}
+
+function watch(done) {
+    browserSync.init({
+        proxy: "http://testyt.local",
+    });
+
+    // Watch for changes in the source SASS / SCSS file
+    gulp.watch("./sass/**/*.scss", styles);
+    gulp.watch("./dist/main.css").on("change", browserSync.reload);
+
+    done();
+}
+
+exports.watch = watch;
